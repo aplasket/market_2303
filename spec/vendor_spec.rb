b9cb9aa2a98dd52dd1d19e204523174f157d50f9
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-17 11:19:31 -0400

    initialize attributes and tests for vendor class

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -2,12 +2,17 @@ require "spec_helper"
 
 RSpec.describe Vendor do
   before(:each) do
-    
+    @vendor = Vendor.new("Rocky Mountain Fresh")
   end
 
   describe "#initialize" do
     it "exists and has attributes" do
-      
+      expect(@vendor).to be_a(Vendor)
+      expect(@vendor.name).to eq("Rocky Mountain Fresh")
+    end
+
+    it "starts with an empty hash of inventory" do
+      expect(@vendor.inventory).to eq({})
     end
   end
 end
