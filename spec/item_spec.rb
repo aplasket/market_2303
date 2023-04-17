cbc4c1cccf860fcda519dda1255960b8ad022e32
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-17 11:06:54 -0400

    initialize item class

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,1 +1,15 @@
 require 'rspec'
+require 'spec_helper'
+
+RSpec.describe Item do
+  before(:each) do
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  describe "#initialize" do
+    it "exists and has attributes" do
+      
+    end
+  end
+end
