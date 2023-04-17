1bb4bf96aa07a283f5280c51ca0cec84fcbcc00e
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-17 12:53:33 -0400

    build out assertions for market

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe Market do
       @market.add_vendor(@vendor1)
       @market.add_vendor(@vendor2)
       @market.add_vendor(@vendor3)
-      
+
       expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
     end
   end
@@ -71,6 +71,50 @@ RSpec.describe Market do
       expect(@vendor1.potential_revenue).to eq(29.75)
       expect(@vendor2.potential_revenue).to eq(345.00)
       expect(@vendor3.potential_revenue).to eq(48.75)
+    end
+  end
+
+  # @item1 = Item.new({name: 'Peach', price: "$0.75"})
+  # @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+  # @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+  # @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+  describe "#sorted_item_list" do
+    xit "returns a sorted array of all items vendors have in stock" do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+
+      expect(@market.sorted_item_list).to eq(["Banana Nice Cream", 'Peach', "Peach-Raspberry Nice Cream", 'Tomato'])
+    end
+  end
+
+  describe "#total inventory" do
+    xit "returns a hash of all items sold at market with hash values" do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+      expect(@vendor1.inventory).to eq({ @item1 => 35,
+                                        @item2 => 7})
+      expect(@vendor2.inventory).to eq({ @item4 => 50,
+                                        @item3 => 25})
+      expect(@vendor3.inventory).to eq({ @item1 => 65})
+      2.times {@vendor1.sell(@item1)}
+      5.times {@vendor1.sell(@item2)}
+      4.times {@vendor2.sell(@item4)}
+      4.times {@vendor2.sell(@item3)}
+      7.times {@vendor3.sell(@item1)}
+
+      expect(@market.total_inventory).to eq({@item1 => {9: }})
+
+    end
+  end
+
+  describe "#overstocked items" do
+    xit "returns an array of Item objects that are overstocked" do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
     end
   end
 end
