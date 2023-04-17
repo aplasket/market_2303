2a832aac1586198c1406598732b42c1e30ba4c9b
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-17 11:17:00 -0400

    initialize test for vendor class

---
+++ spec/vendor_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+RSpec.describe Vendor do
+  before(:each) do
+    
+  end
+
+  describe "#initialize" do
+    it "exists and has attributes" do
+      
+    end
+  end
+end
